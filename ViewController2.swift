31dad733e0bd521acf139995f90743194a845b1a
Tanya Tomchuk <tanyem@protonmail.com>
2016-08-04 23:09:52 +0300

    added the movement of characters on the playing field

--- ViewController2.swift
+++ ViewController2.swift
@@ -7,6 +7,10 @@
 //
 
 import UIKit
+var dicep1 = 0
+var dicep2 = 0
+
+
 
 class ViewController2: UIViewController {
     
@@ -16,10 +20,18 @@ class ViewController2: UIViewController {
     let pers2i: UIImage = UIImage(named: "jigglypuff.png")!
     let pers2v: UIImageView = UIImageView()
     
+    var x: [Int] = [0,40,80,120,160,200,240,280,280,240,200,160,120,80,40,0,0,40,80,120,160,200,240,280,280,240,200,160,120,80,40,0,0,40,80,120,160,200,240,280,280,240,200,160,120,80,40,0,0,40,80,120,160,200,240,280,280,240,200,160,120,80,40,0]
+    var y: [Int] = [300,300,300,300,300,300,300,300,260,260,260,260,260,260,260,260,220,220,220,220,220,220,220,220,180,180,180,180,180,180,180,180,140,140,140,140,140,140,140,140,100,100,100,100,100,100,100,100,60,60,60,60,60,60,60,60,20,20,20,20,20,20,20,20]
+    
+    
     func pers1_view() {
         pers1v.image = pers1i
         pers1v.frame.size.width = 40
         pers1v.frame.size.height = 40
+        UIView.animateWithDuration(0.8, animations: {
+            self.pers1v.frame.origin.x = CGFloat(self.x[dicep1])
+            self.pers1v.frame.origin.y = CGFloat(self.y[dicep1])
+        })
         view.addSubview(pers1v)
     }
     
@@ -27,6 +39,10 @@ class ViewController2: UIViewController {
         pers2v.image = pers2i
         pers2v.frame.size.width = 40
         pers2v.frame.size.height = 40
+        UIView.animateWithDuration(0.8, animations: {
+            self.pers2v.frame.origin.x = CGFloat(self.x[dicep2])
+            self.pers2v.frame.origin.y = CGFloat(self.y[dicep2])
+        })
         view.addSubview(pers2v)
     }
     
@@ -42,7 +58,14 @@ class ViewController2: UIViewController {
         
     }
     @IBAction func go(sender: UIButton) {
-        var clic_go = DiceGo()
+        dicep1 += DiceGo()
+        if (dicep1 < 63) {
+            pers1_view()
+        }
+        dicep2 += DiceGo()
+        if (dicep2 < 63) {
+            pers2_view()
+        }
         
     }
     
